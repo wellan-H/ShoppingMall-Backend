01da6cda66b01bfc0d7146b92da2621044eda67f
wellan <penguin85322@gmail.com>
2022-10-29 08:29:09 +0800

    初始化 Spring Boot Project

---
+++ ShoppingMall-Backend/src/main/java/com/wellan/shoppingmallbackend/ShoppingMallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wellan.shoppingmallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallBackendApplication.class, args);
+    }
+
+}
